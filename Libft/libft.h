edfe34abf851c44f12f3006db91f6811844ac7b9
Tyiuni <mavapisi16@gmail.com>
2026-02-26 23:02:39 +0000

    Strcpy and Strncmp

--- Libft/libft.h
+++ Libft/libft.h
@@ -11,6 +11,9 @@ int     ft_isdigit(int c);
 int     ft_isprint(int c);
 int     ft_isascii(int c);
 int     ft_atoi(const char *str);
+char *ft_strcpy(char *dest, const char *src);
+int ft_strncmp(const char *s1, const char *s2, unsigned int n);
+
 
 
 
