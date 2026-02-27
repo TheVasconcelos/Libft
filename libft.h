f751103fd9747af528b89b944d03d4bdfd61cde4
Tyiuni <mavapisi16@gmail.com>
2026-02-27 18:52:22 +0000

    feat: add memcpy

--- libft.h
+++ libft.h
@@ -18,7 +18,7 @@ void    ft_bzero(void *s, unsigned int n);
 size_t  ft_strlcat(char *dst, const char *src, size_t dstsize);
 char    *ft_strdup(const char *src);
 char    *ft_substr(char const *s, unsigned int start, size_t len);
-
+void *memcpy(void *dest, const void *src, size_t n);
 
 
 
