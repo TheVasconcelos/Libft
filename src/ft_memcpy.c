55466587349286f979b3aa9a2a8c14c2ff62adbf
Tyiuni <mavapisi16@gmail.com>
2026-02-27 18:58:05 +0000

    refactor: add memcpy

--- src/ft_memcpy.c
+++ src/ft_memcpy.c
@@ -5,8 +5,11 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
 	size_t	i;
 
-	i = -1;
-	while (++i < n)
-		((char *)dest)[i] = ((char *)src)[i];
+	i = 0;
+	while (i < n)
+    {
+		((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
+        i++;
+    }
 	return (dest);
 }
