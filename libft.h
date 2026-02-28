56b5c71cdad912a62aacc7197ffef1e0cd3bd0e8
Tyiuni <mavapisi16@gmail.com>
2026-02-28 18:07:35 +0000

    refactor: functions by category for libft.h

--- libft.h
+++ libft.h
@@ -3,30 +3,37 @@
 #define LIBFT_H
 
 #include <stddef.h>
+#include <stdlib.h>
 
-size_t		ft_strlen(const char	*str);
+// Chars 
 int     ft_isalnum(int c);
-int		ft_isalpha(int	c);
+int     ft_isalpha(int c);
 int     ft_isdigit(int c);
 int     ft_isprint(int c);
 int     ft_isascii(int c);
-int     ft_atoi(const char *str);
-char *ft_strcpy(char *dest, const char *src);
-int ft_strncmp(const char *s1, const char *s2, unsigned int n);
-void    *ft_memset(void *b, int c, unsigned int len);
-void    ft_bzero(void *s, unsigned int n);
+int     ft_toupper(int c);
+int     ft_tolower(int c);
+
+// Strings 
+size_t  ft_strlen(const char *str);
+char    *ft_strcpy(char *dest, const char *src);
+int     ft_strncmp(const char *s1, const char *s2, unsigned int n);
 size_t  ft_strlcat(char *dst, const char *src, size_t dstsize);
 char    *ft_strdup(const char *src);
 char    *ft_substr(char const *s, unsigned int start, size_t len);
-void *memcpy(void *dest, const void *src, size_t n);
-void    *ft_memmove(void *dest, const void *src, size_t n);
-int ft_memcmp(const void *s1, const void *s2, size_t n);
 char    *ft_strchr(const char *s, int c);
-void    *ft_memchr(const void *s, int c, size_t n);
 char    *ft_strrchr(const char *s, int c);
-void *ft_calloc(size_t nmemb, size_t size);
-int ft_toupper (int c);
 
+// Memory
+void    *ft_memset(void *b, int c, unsigned int len);
+void    ft_bzero(void *s, unsigned int n);
+void    *ft_memcpy(void *dest, const void *src, size_t n);
+void    *ft_memmove(void *dest, const void *src, size_t n);
+void    *ft_memchr(const void *s, int c, size_t n);
+int     ft_memcmp(const void *s1, const void *s2, size_t n);
 
+// Conversion // Allocation 
+int     ft_atoi(const char *str);
+void    *ft_calloc(size_t nmemb, size_t size);
 
 #endif
