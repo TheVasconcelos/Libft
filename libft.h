d1a784ac11a3b918460344b296d362ffb383d207
Tyiuni <mavapisi16@gmail.com>
2026-02-28 18:22:09 +0000

    feat: add strjoin

--- libft.h
+++ libft.h
@@ -23,6 +23,8 @@ char    *ft_strdup(const char *src);
 char    *ft_substr(char const *s, unsigned int start, size_t len);
 char    *ft_strchr(const char *s, int c);
 char    *ft_strrchr(const char *s, int c);
+char *ft_strjoin(char const *s1, char const *s2);
+
 
 // Memory
 void    *ft_memset(void *b, int c, unsigned int len);
