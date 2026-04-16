d1b5fba743d2211806e68fb94485e9e5d37c8608
Tyiuni <mavapisi16@gmail.com>
2026-04-16 22:06:58 +0100

    feat: add strmapi

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -1,1 +1,23 @@
+#include "libft.h"
 
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char		*result;
+	size_t		i;
+	size_t		size;
+
+	if (!s || !f)
+		return (NULL);
+	size = ft_strlen(s) + 1;
+	result = malloc(size);
+	if (!result)
+		return (NULL);
+	i = 0;
+	while (i < size)
+	{
+		result[i] = f(i, s[i]);
+		i++;
+	}
+	result[i] = '\0';
+	return (result);
+}
